ee7707228d712400d31ad05b8a963d0dd2d1d520
hieudnguyen <hieund898@gmail.com>
2025-04-02 22:38:04 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { FormValidationComponent } from './pages/form-validation/form-validation.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home page',
+    component: HomeComponent,
+  },
+  {
+    path: 'form-validation',
+    title: 'Form Validation page',
+    component: FormValidationComponent,
+  },
+];
---
+++ src/app/pages/form-validation/form-validation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-validation',
+  imports: [],
+  templateUrl: './form-validation.component.html',
+  styleUrl: './form-validation.component.scss'
+})
+export class FormValidationComponent {
+
+}
