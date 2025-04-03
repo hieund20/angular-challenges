6eaeb41b92a916ed9f8ebcc9a166b7c6a3c4d63d
hieudnguyen <hieund898@gmail.com>
2025-04-03 21:52:15 +0700

    feat: add form validation

--- src/app/pages/form-validation/form-validation.component.ts
+++ src/app/pages/form-validation/form-validation.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form-validation',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-validation.component.html',
-  styleUrl: './form-validation.component.scss'
+  styleUrl: './form-validation.component.scss',
 })
 export class FormValidationComponent {
+  isSubmit: boolean = false;
 
+  profileForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    userName: new FormControl<string>('', Validators.required),
+    phone: new FormControl<string>('', Validators.required),
+  });
+
+  handleSubmit() {
+    console.log(this.profileForm.controls.email);
+    this.isSubmit = true;
+  }
 }
